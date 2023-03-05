d3b140b29c1d4dde4dc9905c262c0f8b3b43e78a
tscobarr <124937392+tscobarr@users.noreply.github.com>
2023-03-05 16:51:46 -0500

    Test4

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,7 +2,7 @@ package classroom;
 
 public class Persona {
 
-    long cedula;
+    long cedula = 1;
     String nombre;
     static int totalPersonas;
     
@@ -30,13 +30,15 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
-        this.cedula = 1;
         totalPersonas++;
     }
     
     public Persona() {
-    	this(0);
+        this.nombre = "";
+        this.cedula = 0;
+        totalPersonas++;
     }
+    
     
     public long getCedula() {
         return cedula;
